d7775462192c0e50f3012213bdad616e9a4f5e52
ahmed-jheer <ahmedjj800@gmail.com>
2026-05-31 09:01:52 +0300

    rename logo image to jhear

--- app/Components/global/Logo.tsx
+++ app/Components/global/Logo.tsx
@@ -3,7 +3,7 @@ export const Logo = () => {
   return (
 
      <div className="relative h-10 w-20 cursor-pointer hover:opacity-80 transition flex gap-5  rounded-md  z-10 ">
-              <Image alt="JheAr" src="/logo.png" fill className="object-cover text-red-500 p-2 font-bold bg-transparent z-50 " />
+              <Image alt="JheAr" src="/jhear.png" fill className="object-cover text-red-500 p-2 font-bold bg-transparent z-50 " />
             </div>
   );
 };
